a6d0fde03edb7b8882f8075511597d348b1c015b
Alexandr <kaperisatiom@mail.ru>
2023-10-29 20:54:11 +0300

    ht3

--- src/main/java/ht3/Ht3.java
+++ src/main/java/ht3/Ht3.java
@@ -3,13 +3,13 @@ package ht3;
 public class Ht3 {
     // 1. Нужно покрыть тестами метод на 100%
     // Метод проверяет, является ли целое число записанное в переменную n, чётным (true) либо нечётным (false).
-    public static boolean evenOddNumber(int num) {
+    public boolean evenOddNumber(int num) {
         return num % 2 == 0;
     }
 
     // 2. Нужно написать метод который проверяет, попадает ли переданное число в интервал (25;100)
     // и возвращает true,если попадает и false - если нет, покрыть тестами метод на 100%
-    public static boolean numberInInterval(int num) {
+    public boolean numberInInterval(int num) {
         return (num > 25 && num < 100);
     }
 }
